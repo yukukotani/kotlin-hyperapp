461694871611b850457230761aafcf8ae6311e0a
monchi <poyo0315@gmail.com>
2019-07-19 07:24:31 +0900

    Apply release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("org.jetbrains.kotlin.js") version "1.3.41"
     id("com.jfrog.bintray") version "1.8.4" apply false
+    id("net.researchgate.release") version "2.8.1"
 }
 
 allprojects {
-    group = "land.mog"
-    version = "1.1.0"
-    
-    ext["kotlinVersion"] = "1.3.41"
-    ext["hyperappVersion"] = "1.2.10"
+    group = rootProject.group
+    version = rootProject.version
+}
+
+release {
+    failOnCommitNeeded = false
+    failOnUnversionedFiles = false
 }
